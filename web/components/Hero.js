ee255ed19050adf911204239d6b662a61271daaa
Carlos Yarza <carlosyarza@gmail.com>
2022-03-16 15:31:56 +0100

    fix(sintax): add closing div to hero

--- web/components/Hero.js
+++ web/components/Hero.js
@@ -7,7 +7,11 @@ import Oddysey from "./svgs/OrbitOddysey.svg";
 const Hero = () => {
   return (
     <div className="bg-white">
+<<<<<<< HEAD
       <div className="z-30 mt-36 w-full pl-96 center hidden sm:flex font-magistrat-light italic xs:text-transparent sm:text-black text-3xl filter invert  mix-blend-difference flex justify-center absolute">
+=======
+      <div className="z-30 mt-36 w-full ml-72 font-magistrat-light italic xs:text-transparent sm:text-black text-3xl filter invert  mix-blend-difference flex justify-center absolute">
+>>>>>>> fix(sintax): add closing div to hero
         <p>/website under construction</p>
       </div>
       <div className=" flex items-center justify-around relative ">
@@ -32,7 +36,16 @@ const Hero = () => {
           </button>
         </div>
       </div>
+<<<<<<< HEAD
     </div>
+=======
+      <div className="z-20 flex flex-col justify-items-center items-center relative">
+        <h2>Veit Schmidleitner</h2>
+        <h3>ORBIT ODDYSEY</h3>
+        <p>March 18 - June 19, 2022</p>
+      </div>
+    </div>  
+>>>>>>> fix(sintax): add closing div to hero
   );
 };
 
