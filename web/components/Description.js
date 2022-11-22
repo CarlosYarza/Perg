c13776b3f3ec4e0f408b0f5e44ad315d65492700
Carlos Yarza <carlosyarza@gmail.com>
2022-11-22 23:51:10 +0100

    change justify left

--- web/components/Description.js
+++ web/components/Description.js
@@ -1,8 +1,8 @@
 const Description = () => {
   return (
     <div className="z-20 flex flex-col justify-items-center items-center relative font-magistrat text-xs sm:text-md md:text-xl md:pb-12">
-      <h2>Opening  ⎯ Dec 01, 6-11PM </h2>
-      <h2>Duration ⎯ Dec 01, 2022 - Jan 28, 2023</h2>
+      <h2>Opening  ⎯ Dec 01, 6-11PM <br></br>Duration ⎯ Dec 01, 2022 - Jan 28, 2023</h2>
+      
     </div>
   );
 };
